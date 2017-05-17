2427eac4cbf3b4c50e2948a8e2536d2e08d8b0b0
Kirill Osenkov <github@osenkov.com>
2017-05-17 11:08:06 -0700

    Initial commit.

---
+++ AppDomainManagerTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Policy;
+
+namespace AppDomainManagerTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"Default domain: {AppDomain.CurrentDomain.Id}");
+			var newDomain = AppDomain.CreateDomain("New Domain", new Evidence(AppDomain.CurrentDomain.Evidence), AppDomain.CurrentDomain.BaseDirectory, null, false);
+			newDomain.DoCallBack(RunInAnotherDomain);
+		}
+
+		static void RunInAnotherDomain()
+		{
+			Console.WriteLine($"Running in appdomain: {AppDomain.CurrentDomain.Id}");
+		}
+	}
+
+    public class CustomAppDomainManager : AppDomainManager
+    {
+        public override void InitializeNewDomain(AppDomainSetup appDomainInfo)
+        {
+            Console.WriteLine($"Initializing new AppDomain");
+            base.InitializeNewDomain(appDomainInfo);
+            appDomainInfo.AppDomainInitializer = new AppDomainInitializer(InitializeNewAppDomain);
+        }
+
+        public static void InitializeNewAppDomain(string[] args)
+        {
+            //Console.WriteLine("AppDomain id: " + AppDomain.CurrentDomain.Id);
+            //Console.WriteLine(string.Join(" ", args));
+        }
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDomainManagerTest
+{
+	public class CustomAppDomainManager : AppDomainManager
+	{
+		public override void InitializeNewDomain(AppDomainSetup appDomainInfo)
+		{
+			Console.WriteLine($"Initializing new AppDomain");
+			base.InitializeNewDomain(appDomainInfo);
+            appDomainInfo.AppDomainInitializer = new AppDomainInitializer(InitializeNewAppDomain);
+        }
+
+		public static void InitializeNewAppDomain(string[] args)
+		{
+            //Console.WriteLine("AppDomain id: " + AppDomain.CurrentDomain.Id);
+            //Console.WriteLine(string.Join(" ", args));
+        }
+	}
+}
